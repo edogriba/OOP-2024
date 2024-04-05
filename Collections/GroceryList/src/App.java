c09e181a6abe66cee8bb7e8f293032f74fb13049
edogriba <76633735+edogriba@users.noreply.github.com>
2024-04-05 11:24:45 +0200

    Understood how to use sets

--- Collections/GroceryList/src/App.java
+++ Collections/GroceryList/src/App.java
@@ -1,5 +1,48 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Set<String> cinnamonRolls = new HashSet<>();
+        cinnamonRolls.add("Flour");
+        cinnamonRolls.add("Milk");
+        cinnamonRolls.add("Butter");
+        cinnamonRolls.add("Sugar");
+        cinnamonRolls.add("Salt");
+        cinnamonRolls.add("Cinnamon");
+        cinnamonRolls.add("Yeast");
+        cinnamonRolls.add("Yeast");
+
+        System.out.println(cinnamonRolls.toString());
+
+        Set<String> atHome = new HashSet<>();
+        atHome.add("Flour");
+        atHome.add("Eggs");
+        atHome.add("Butter");
+        atHome.add("Cookies");
+        atHome.add("Salt");
+        atHome.add("Cocoa powder");
+        atHome.add("Raisin");
+        atHome.add("Almonds");
+
+        System.out.println(atHome.toString());
+
+        Set<String> grocerySet = new HashSet<String>();
+
+        cinnamonRolls.forEach(
+            x ->
+            {
+                if (!(atHome.contains(x))) {
+                    grocerySet.add(x);
+                }
+            }
+        );
+        grocerySet.forEach(System.out::println);
+
+        
+
+
+
     }
 }
