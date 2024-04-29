851c270ea1a5d908af1daf4a5552a8833bcb9e1f
edogriba <76633735+edogriba@users.noreply.github.com>
2024-04-29 17:26:08 +0200

    Tried out some methods to solve the exercise's requests

--- Stream/Inventory/src/App.java
+++ Stream/Inventory/src/App.java
@@ -1,8 +1,8 @@
 import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -18,7 +18,7 @@ public class App {
             public String getDate() {
                 return this.date;
             }
-            public int getcost() {
+            public int getCost() {
                 return this.cost;
             }
         }
@@ -57,21 +57,61 @@ public class App {
         Order o3 = new Order("19/11/2023", 549);
         Order o4 = new Order("14/01/2024", 5);
         Product p1 = new Product("Calze");
-        p1.addOrder(o1);
-        p1.addOrder(o3);
+        //p1.addOrder(o1);
+        //p1.addOrder(o3);
         Product p2= new Product("Maglietta");
-        p2.addOrder(o2);
+        //p2.addOrder(o2);
         Product p3= new Product("Scarpe");
         //p3.addOrder(o4);
         inventoryList.add(p1);
         inventoryList.add(p2);
         inventoryList.add(p3);
 
+        // Sorted list of elements in the inventory
+
         inventoryList.stream().sorted(Comparator.comparing(Product::getName)).map(Product::getName).forEach(System.out::println);
+        
+        // Check if there is at least 1 request for each product 
         inventoryList.stream().filter(x-> x.getNumOrders() > 0 ).sorted(Comparator.comparing(Product::getName)).map(Product::getName).forEach(System.out::println);
+        
+        // Count how many products have more than 1 order
         long count = inventoryList.stream().filter(x-> x.getNumOrders() > 0 ).sorted(Comparator.comparing(Product::getName)).map(Product::getName).collect(Collectors.counting());
         System.out.println(count);
+
+        // Most requested product
+
         List<String> lista = inventoryList.stream().filter(x-> x.getNumOrders() > 0 ).sorted(Comparator.comparing(Product::getNumOrders).reversed()).map(Product::getName).collect(Collectors.toList());
-        System.out.println(lista.get(0));
+        if (!lista.isEmpty()) {
+            System.out.println(lista.get(0));
+        }
+        else {
+            System.out.println("Nessun ordine effettuato in nessuno dei prodotti");
+        }
+
+        // Most requested product professor's version 1
+
+        Optional<Product> p = inventoryList.stream().max(Comparator.comparing(Product::getNumOrders));
+        System.out.println(p.get().getName());
+
+        // Most requested product professor's version 2 (using just one single stream)
+
+        // [...]
+
+        // Most requested product professor's version 3
+
+        Optional<Product> productOptional = inventoryList.stream().filter(x->x.getNumOrders()!=0).max(Comparator.comparing(Product::getNumOrders));
+        
+        if (!productOptional.isPresent()){
+            System.out.println("No nothing");
+        }
+        else {
+            System.out.println(productOptional.get().getName());
+        }
+        
+
+
+        //mostReq = products.stream();
+
+
     }
 }
